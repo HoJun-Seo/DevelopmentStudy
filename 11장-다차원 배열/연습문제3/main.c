7f14b011bdc4aa1ade4c69a24c3946a4b011ee31
SeoHoJun <ghwns6659@naver.com>
2020-01-10 00:00:44 +0900

    multi-dimensional array

--- 11장-다차원 배열/연습문제3/main.c
+++ 11장-다차원 배열/연습문제3/main.c
@@ -4,6 +4,7 @@ int main(void)
     int i = 0, j = 0;
     int row = 0, col = 0;
     int array[9][9];
+
     for(i = 1; i < 10; i++){
         for(j = 1; j < 10; j++){
             array[col][row] = i * j;
