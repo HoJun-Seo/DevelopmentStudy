5a03f02d985ca98f40fa9e312d525be2a54b37d1
SeoHoJun <ghwns6659@naver.com>
2020-01-19 01:26:03 +0900

    C pointer and array

---
+++ 13장-포인터와 배열/학습용-포인터 변수의 상수화2/main.c
@@ -0,0 +1,21 @@
+//포인터 변수를 통해 메모리 공간의 값을 변경하지 못하게 한다.
+#include<stdio.h>
+int main(void)
+{
+    char a = 'A';
+    char b = 'B';
+    const char* p = &a; //*p를 상수화
+
+    printf("%c \n", *p);
+    printf("%c \n", a);
+
+    p = &b;
+    printf("%c \n", *p);
+    printf("%c \n", b);
+
+    a = 'X';
+    b = 'C';
+    printf("%c %c\n", b, *p);
+
+    return 0;
+}
